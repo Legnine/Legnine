1e9eaf3c481bcc57483e50e745cdd1267656beb1
Legnine <116827005+Legnine@users.noreply.github.com>
2023-03-03 14:10:48 -0500

    Update metrics.js

--- metrics.js
+++ metrics.js
@@ -1,1 +1,13 @@
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+const legnine = new Legnine({
+  auth: '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'
+})
 
+await octokit.request('GET /repos/{owner}/{repo}/community/profile', {
+  owner: 'Legnine',
+  repo: 'Legnine',
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
